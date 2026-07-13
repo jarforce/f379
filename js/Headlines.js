6f8b69712ae55449334658b876fe260cf177879a
Jarforce <35603960+jarforce@users.noreply.github.com>
2026-07-13 08:19:57 +0700

    Shorten link text from 'More news' to 'More'

--- js/Headlines.js
+++ js/Headlines.js
@@ -845,7 +845,7 @@ const Headlines = {
 
 				if (Feeds.infscroll_disabled)
 					hsp.innerHTML = "<a href='#' onclick='Feeds.reloadCurrent()'>" +
-						__("More news") + "</a>";
+						__("More") + "</a>";
 
 				/*
 				if (Feeds._search_query) {
